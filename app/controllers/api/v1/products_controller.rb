b04c61ee6b195eb56b04f48f5a5702266eb7fe46
Josh <Joshua.horgen@gmail.com>
2020-01-12 15:02:02 -0800

    Add routes for charges controller & add dotenv-rails gem for temporary api key protection

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::ProductsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  # GET /products
   def index
     @products = Product.order("created_at DESC")
     .find_by_category(params[:category])
@@ -9,13 +8,7 @@ class Api::V1::ProductsController < ApplicationController
     .find_by_title(params[:title])
     render json: @products
   end
-  # GET /products/1
-  def show
-    render json: @product
-  end
 
-
-  # POST /products
   def create
     @product = Product.create(product_params)
     render json: @product
@@ -25,7 +18,6 @@ class Api::V1::ProductsController < ApplicationController
     params.require(:product).permit(:category, :brand, :description, :color1, :color2, :color4, :color5, :size1, :size2, :size3, :size4, :size5, :quantity, :price, :saleprice, :image1, :image2, :image3, :image4, :image5, :review, :rating, :review, :rating, :checkoutquantity, :title)
   end
 
-  # PATCH/PUT /products/1
   def update
     @product = Product.find(params[:id])
     if @product.update_attributes(product_params)
@@ -34,7 +26,7 @@ class Api::V1::ProductsController < ApplicationController
       render json: @product.errors, status: :unprocessable_entity
     end
   end
-  # DELETE /products/1
+
   def destroy
     @product = Product.find(params[:id])
     if @product.destroy
@@ -43,12 +35,4 @@ class Api::V1::ProductsController < ApplicationController
       render json: @product.errors, status: :unprocessable_entity
     end
   end
-
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_product
-    @product = Product.find(params[:id])
-  end
-  # Only allow a trusted parameter “white list” through.
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
    resources :products
   end
  end
+ namespace :api do
+  namespace :v1 do
+   resources :charges
+  end
+ end
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
