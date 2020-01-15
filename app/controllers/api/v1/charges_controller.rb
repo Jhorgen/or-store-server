cdf558f66bc9595542bb976d87cc1575295afcaf
Josh <Joshua.horgen@gmail.com>
2020-01-15 02:35:28 -0800

    Add scopes for stripe api.

--- app/controllers/api/v1/charges_controller.rb
+++ app/controllers/api/v1/charges_controller.rb
@@ -9,19 +9,32 @@ class Api::V1::ChargesController < ApplicationController
 
     begin
 
-      customer = Stripe::Customer.create(
-        :source => params[:charge][:token]
-      )
+      customer = Stripe::Customer.create({
+        :source => params[:charge][:token],
+        :email => params[:charge][:email],
+        :description => params[:charge][:description]
+      })
 
       charge = Stripe::Charge.create({
         :customer => customer.id,
         :amount => params[:charge][:amount],
         :currency => params[:charge][:currency],
+        :billing_details => params[:email]
         })
 
         rescue Stripe::CardError => e
           render json: { message: 'oops'}, status: :not_acceptable
 
     end
+    
   end
+
+  def index
+    Stripe.api_key = ENV["EXAMPLE_API_KEY"]
+
+    @charges = Stripe::Customer.list({email: params[:email]})
+
+    render json: @charges
+  end
+
 end
