ddc70b50bf92ab53bc3ded727c4a1f8be1dc8307
Josh <Joshua.horgen@gmail.com>
2020-01-20 14:55:37 -0800

    Set up action mailer for purchase receipts.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,8 +30,11 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
+  config.action_mailer.perform_deliveries = true
+
+
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -43,6 +46,9 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
+
+  config.action_mailer.delivery_method = :smtp
+
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
