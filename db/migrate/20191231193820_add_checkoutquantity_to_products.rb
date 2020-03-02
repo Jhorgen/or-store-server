4409a65e40cbf5a8c9b3b6bcb9226142e3fcb051
Josh <Joshua.horgen@gmail.com>
2020-03-01 17:30:33 -0800

    Delete completed migrations.

--- db/migrate/20191231193820_add_checkoutquantity_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AddCheckoutquantityToProducts < ActiveRecord::Migration[5.2]
-  def change
-    add_column :products, :checkoutquantity, :integer
-  end
-end
--- db/migrate/20200120224750_add_selected_option_index_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AddSelectedOptionIndexToProducts < ActiveRecord::Migration[5.2]
-  def change
-    add_column :products, :selectedOptionIndex, :string
-  end
-end
