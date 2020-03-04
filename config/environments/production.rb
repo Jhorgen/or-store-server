2fd6a9bdb98425f03457f2c987aaf83551e0df36
Josh <Joshua.horgen@gmail.com>
2020-03-03 18:59:30 -0800

    Add heroku URL as asset host.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,8 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = 'https://nameless-hollows-85718.herokuapp.com/'
+  config.action_mailer.asset_host = config.action_controller.asset_host
+
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
