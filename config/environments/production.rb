3426d71b6473565ed8eca4ceb11b659cda6c7038
Josh Horgen <39712421+Jhorgen@users.noreply.github.com>
2022-02-16 16:08:21 -0800

    Config fix.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = 'https://secure-spire-22048.herokuapp.com/'
+  config.action_controller.asset_host = 'https://sheltered-escarpment-22688.herokuapp.com/'
   config.action_mailer.asset_host = config.action_controller.asset_host
 
 
